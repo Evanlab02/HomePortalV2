b6e6b839bbd0560217a83ddbb3084a87d0087c95
Evanlab02 <evanlabuschagne70@gmail.com>
2025-10-24 22:06:38 +0200

    fix: Various docker, script and compose issues

--- apps/homeportal/scripts/admin.start.sh
+++ apps/homeportal/scripts/admin.start.sh
@@ -5,4 +5,4 @@ echo "Running migrations..."
 python manage.py migrate
 
 echo "Starting admin server..."
-gunicorn -b 0.0.0.0:80 -w 1 --log-config app/logging.config --capture-output --log-level info 'app.wsgi:application'
+gunicorn -b 0.0.0.0:80 -w 1 --log-config app/settings/logging.config --capture-output --log-level info 'app.wsgi:application'
