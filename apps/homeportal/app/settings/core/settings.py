fbebe7a84845e80a93ae91a0a7710404836590a5
Evanlab02 <evanlabuschagne70@gmail.com>
2025-11-14 23:00:48 +0200

    fix(Logging): Try to add more comprehensive logging

--- apps/homeportal/app/settings/core/settings.py
+++ apps/homeportal/app/settings/core/settings.py
@@ -280,3 +280,24 @@ UNFOLD = {
     #     lambda request: "static/js/custom.js",
     # ],
 }
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "[{levelname}] {asctime} {name} - {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+}
